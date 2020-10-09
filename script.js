942ccbd2c35efc578426ae60d6d5e4f48bdf2520
connietee22 <connietsangphotos@gmail.com>
2020-10-09 00:00:10 -0400

    set sticky nav and hamburger menu

---
+++ script.js
@@ -0,0 +1,22 @@
+$(function () {
+	$('.hamburger').click(function () {
+		// toggle class show on the menu
+		$('.menu').addClass('show');
+	});
+
+	$('.hamburgerExit').click(function () {
+		// toggle class show on the menu
+		$('.menu').removeClass('show');
+	});
+
+	const navOffset = $('.navBg').offset().top;
+	let windowScroll = $(document).scrollTop();
+
+	if (windowScroll >= navOffset) {
+		$('.navBg').addClass('sticky');
+	} else {
+		$('.navBg').removeClass('sticky');
+	}
+	console.log(navOffset);
+	console.log(windowScroll);
+});
