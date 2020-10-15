c96b616f5dae46f95d7553876f045d409a7e0054
connietee22 <connietsangphotos@gmail.com>
2020-10-15 12:36:43 -0400

    copy tweaks

--- script.js
+++ script.js
@@ -6,28 +6,8 @@ $menu = $('.menu');
 $hamburgExit = $('.hamburgerExit');
 $navBg = $('.navBg');
 
-portfolio.init = () => {
-	$hamburger.click(function () {
-		// toggle class show on the menu
-		$menu.addClass('show');
-		$hamburger.removeClass('display');
-	});
-
-	$hamburgExit.click(function () {
-		// toggle class show on the menu
-		$menu.removeClass('show');
-		$hamburger.addClass('display');
-	});
-
-	// collapsing menu when option clicked
-	const screenWidth = $('body').width();
-	if (screenWidth < 769) {
-		$('.menuScreen a').click(function () {
-			$menu.removeClass('show');
-		});
-	}
-
-	// ON WINDOW SCROLL---------------------------------------------------------------//
+// triggering the sticky nav on window scroll
+portfolio.triggerNav = () => {
 	$(window).scroll(function () {
 		if ($(this).scrollTop() > 1) {
 			$navBg.addClass('sticky');
@@ -35,6 +15,71 @@ portfolio.init = () => {
 			$navBg.removeClass('sticky');
 		}
 	});
+};
+
+// portfolio.watchForHover = () => {
+// 	// lastTouchTime is used for ignoring emulated mousemove events
+// 	let lastTouchTime = 0;
+
+// 	function enableHover() {
+// 		if (new Date() - lastTouchTime < 500) return;
+// 		document.body.classList.add('hasHover');
+// 	}
+
+// 	function disableHover() {
+// 		document.body.classList.remove('hasHover');
+// 	}
+
+// 	function updateLastTouchTime() {
+// 		lastTouchTime = new Date();
+// 	}
+
+// 	document.addEventListener('touchstart', updateLastTouchTime, true);
+// 	document.addEventListener('touchstart', disableHover, true);
+// 	document.addEventListener('mousemove', enableHover, true);
+
+// 	enableHover();
+// };
+
+// showing nav items on hamburger button click
+portfolio.burgerDropdown = () => {
+	$hamburger.click(function () {
+		// toggle class show on the menu
+		$menu.addClass('show');
+		$hamburger.removeClass('display');
+	});
+};
+
+// removing dropdown when the exit button is clicked
+portfolio.removeDropdown = () => {
+	$hamburgExit.click(function () {
+		// toggle class show on the menu
+		$menu.removeClass('show');
+		$hamburger.addClass('display');
+	});
+};
+
+// collapsing menu when nav link is clicked
+portfolio.onDropdownClick = () => {
+	const screenWidth = $('body').width();
+	if (screenWidth < 769) {
+		$('.menuScreen a').click(function () {
+			$menu.removeClass('show');
+		});
+	}
+};
+
+portfolio.init = () => {
+	// showing nav items on button click
+	portfolio.burgerDropdown();
+	// removing dropdown when the exit button is clicked
+	portfolio.removeDropdown();
+	// collapsing menu when nav link is clicked
+	portfolio.onDropdownClick();
+	// Triggering sticky nav on window scroll
+	portfolio.triggerNav();
+
+	// portfolio.watchForHover();
 };
 
 $(function () {
